58e6442fbb45a7b22216bad67358e8cb1ad5c447
armster01 <armstersaha8@gmail.com>
2025-10-25 19:53:09 +0530

    codes_updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_Chatbot_With_LLMs_LangChain_Pinecone_Flask_AWS",
+    version="1.0.0",
+    author="Adittya Saha",
+    author_email="armstersaha8@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
